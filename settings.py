875b9b22f34d127564d7a077b5ee69b8f3aee7ef
Scott Jones <60559239+Marshall168@users.noreply.github.com>
2022-08-19 18:55:15 +0100

    overlays

--- settings.py
+++ settings.py
@@ -4,3 +4,8 @@ from pygame.math import Vector2
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 TILE_SIZE = 64
+
+OVERLAY_POSITIONS = {
+    'tool' : (40, SCREEN_HEIGHT - 15),
+    'seed' : (70, SCREEN_HEIGHT - 5)
+    }
