368d383577618a7aec3dcec1df378ef10dbd660e
Scott Jones <60559239+Marshall168@users.noreply.github.com>
2022-08-19 17:58:28 +0100

    more animations

--- player.py
+++ player.py
@@ -7,7 +7,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__(group)
 
         self.import_assets()
-        self.status = 'down_axe'
+        self.status = 'down_idle'
         self.frame_index = 0
 
         # general setup
@@ -33,25 +33,40 @@ class Player(pygame.sprite.Sprite):
             self.animations[animation] = import_folder(full_path)
         print(self.animations)
 
+    def animate(self,dt):
+        self.frame_index += 4 * dt
+        if self.frame_index >= len(self.animations[self.status]):
+            self.frame_index = 0
+
+        self.image = self.animations[self.status][int(self.frame_index)]
+
     def input(self):
         keys = pygame.key.get_pressed()
 
         #vert direction
         if keys[pygame.K_UP]:
             self.direction.y = -1
+            self.status = 'up'
         elif keys[pygame.K_DOWN]:
             self.direction.y = 1
+            self.status = 'down'
         else:
             self.direction.y = 0
 
         #horiz direction
         if keys[pygame.K_RIGHT]:
             self.direction.x = 1
+            self.status = 'right'
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.status = 'left'
         else:
             self.direction.x = 0
 
+    def get_status(self):
+        #check if player is moving to add _idle to status
+        if self.direction.magnitude() == 0:
+            self.status += '_idle'
     
     def move(self, dt):
 
@@ -71,3 +86,4 @@ class Player(pygame.sprite.Sprite):
     def update(self, dt):
         self.input()
         self.move(dt)
+        self.animate(dt)
