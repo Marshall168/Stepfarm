86fe1844a091ac6b6119be593e45dfd168fa8c98
Scott Jones <60559239+Marshall168@users.noreply.github.com>
2022-08-19 16:12:21 +0100

    added title

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+        pygame.display.set_caption('Farming')
         self.clock = pygame.time.Clock()
+        
 
     def run(self):
         while True:
