070308f14e3ab0ce625c6c6b1852e2075ed5cbb9
Scott Jones <60559239+Marshall168@users.noreply.github.com>
2022-08-20 11:35:54 +0100

    adding initial background

--- level.py
+++ level.py
@@ -2,6 +2,7 @@ import pygame
 from settings import *
 from player import Player
 from overlay import Overlay
+from sprite import Generic
 
 class Level:
 
@@ -11,16 +12,34 @@ class Level:
         self.display_surface = pygame.display.get_surface()
 
         # sprite groups
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = CameraGroup()
 
         self.setup()
         self.overlay = Overlay(self.player)
 
     def setup(self):
+        Generic(
+            pos = (0,0),
+            surf = pygame.image.load('assets/graphics/world/ground.png').convert_alpha(),
+            groups = self.all_sprites
+            )
+        
         self.player = Player((640,360), self.all_sprites)
 
     def run(self,dt):
        self.display_surface.fill('coral')
-       self.all_sprites.draw(self.display_surface)
+       #self.all_sprites.draw(self.display_surface)
+       self.all_sprites.custom_draw()
        self.all_sprites.update(dt)
        self.overlay.display()
+
+
+class CameraGroup(pygame.sprite.Group):
+
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+
+    def custom_draw(self):
+        for sprite in self.sprites():
+            self.display_surface.blit(sprite.image, sprite.rect)
