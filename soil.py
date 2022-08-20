d2dcaab56d9c67985441e8adc14a2a10ac5ee801
Scott Jones <60559239+Marshall168@users.noreply.github.com>
2022-08-20 16:27:58 +0100

    farmable land 1

--- soil.py
+++ soil.py
@@ -15,10 +15,22 @@ class SoilLayer:
 
 
     def create_soil_grid(self):
-        ground = pygame.image.load('assets/graphics/world.ground.png')
+        ground = pygame.image.load('assets/graphics/world/ground.png')
         h_tiles, v_tiles = ground.get_width() // TILE_SIZE, ground.get_height() // TILE_SIZE
 
         self.grid = [[[] for col in range (h_tiles)] for row in range (v_tiles)]
-        load_pygame('assets/data/map.tmx').get_layer_by_name('Farmable').tiles()
+        for x, y, _ in load_pygame('assets/data/map.tmx').get_layer_by_name('Farmable').tiles():
+            self.grid[y][x].append('F')
+
+
+    def create_hit_rects(self):
+        self.hit_rects = []
+        for index_row, row in enumerate(self.grid):
+            for index_col, cell in enumerate(row):
+                if 'F' in cell:
+                    x = index_col * TILE_SIZE
+                    y = index_row * TILE_SIZE
+                    rect = pygame.Rect(x,y,TILE_SIZE,TILE_SIZE)
+                    self.hit_rects.append(rect)
 
 
