ef0083c055416e6c0bc85b81097de780c523fe4d
Scott Jones <60559239+Marshall168@users.noreply.github.com>
2022-08-20 12:57:15 +0100

    3d layers

--- player.py
+++ player.py
@@ -73,20 +73,20 @@ class Player(pygame.sprite.Sprite):
         if not self.timers['tool use'].active:
 
             #vert direction
-            if keys[pygame.K_UP]:
+            if keys[pygame.K_w]:
                 self.direction.y = -1
                 self.status = 'up'
-            elif keys[pygame.K_DOWN]:
+            elif keys[pygame.K_s]:
                 self.direction.y = 1
                 self.status = 'down'
             else:
                 self.direction.y = 0
 
             #horiz direction
-            if keys[pygame.K_RIGHT]:
+            if keys[pygame.K_d]:
                 self.direction.x = 1
                 self.status = 'right'
-            elif keys[pygame.K_LEFT]:
+            elif keys[pygame.K_a]:
                 self.direction.x = -1
                 self.status = 'left'
             else:
