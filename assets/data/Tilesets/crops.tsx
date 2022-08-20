e7b2752846f0288a18f16510c08cac2ab734a5ce
Scott Jones <60559239+Marshall168@users.noreply.github.com>
2022-08-20 21:00:24 +0100

    playing

---
+++ assets/data/Tilesets/crops.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="crops" tilewidth="64" tileheight="64" tilecount="120" columns="10">
+ <image source="../../cozyfarm/farming/crops64.png" width="640" height="768"/>
+</tileset>
